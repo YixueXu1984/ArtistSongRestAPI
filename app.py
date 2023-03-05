8dc9bfed2e13432577484bcccf9d754db7e0e8de
yixue <v7e0b@ugrad.cs.ubc.ca>
2023-03-04 16:36:13 -0800

    get artists create artist create song get artist get songs

--- app.py
+++ app.py
@@ -1,26 +1,66 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
-
 
 artists = [
     {
         "name": "Bob Dylan",
-        "somgs": [
+        "songs": [
             {
                 "name": "Hurricane",
                 "release year": 1976
             },
             {
                 "name": "One more cup of coffee",
-                "release Year": 1976
+                "release year": 1976
             }
         ]
     }
 ]
 
 
-@app.get("/artists") # http://127.0.0:5000/artists
+@app.get("/artist")  # http://127.0.1:5000/artists
 def get_artists():
     return {"artists": artists}
 
+
+@app.post("/artist")
+def create_artist():
+    request_data = request.get_json()
+    new_artist = {
+        "name": request_data["name"],
+        "songs": []
+    }
+    artists.append(new_artist)
+    return new_artist, 201
+
+
+@app.post("/artist/<string:name>/song")
+def create_song(name):
+    request_data = request.get_json()
+    for artist in artists:
+        if artist["name"] == name:
+            new_song = {
+                "name": request_data["name"],
+                "release year": request_data["release year"]
+            }
+            artist["songs"].append(new_song)
+            return new_song, 201
+
+    return {"message": "Artist not found"}, 404
+
+
+@app.get("/artist/<string:name>")
+def get_artist(name):
+    for artist in artists:
+        if artist["name"] == name:
+            return artist
+    return {"message": "Artist not found"}, 404
+
+
+@app.get("/artist/<string:name>/song")
+def get_songs_by_artist(name):
+    for artist in artists:
+        if artist["name"] == name:
+            return {"songs": artist["songs"]}
+    return {"message": "Artist not found"}, 404
